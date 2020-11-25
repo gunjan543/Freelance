d89c2aa05794542c264422b84b91fe1085e47769
gunjan543 <gunjanagarwal54321@gmail.com>
2020-11-25 21:53:22 +0530

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+ 
+
+function Header() {
+    return (
+      <div className="NavbarItems">
+          <h1 className="navbar-logo">BYOB</h1>
+          <div className="menu-icon">
+
+          </div>
+          <ul>
+              <li><a href=""></a></li>
+          </ul>
+      </div>
+    );
+}
+ 
+export default Header;
