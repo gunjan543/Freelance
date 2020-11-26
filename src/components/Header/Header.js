29edf96ebb87072653ffe93f3aa6af6aca4e3b15
gunjan543 <gunjanagarwal54321@gmail.com>
2020-11-26 17:50:14 +0530

    Added Header-Frontend

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './header.css';
- 
+import { menuItems } from './menu-items';
+import slide1 from './images/1.jpg';
+import slide2 from './images/2.jpg';
+import {Nav,Navbar,Button} from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel'
 
-function Header() {
+class Header extends Component {
+    render()
+    {
     return (
-      <div className="NavbarItems">
-          <h1 className="navbar-logo">BYOB</h1>
-          <div className="menu-icon">
+     <div>
+     <Navbar collapseOnSelect expand="lg" variant="dark">
+     <Navbar.Brand href="#home" bsPrefix ='brand'>BYOG</Navbar.Brand>
+     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+     <Navbar.Collapse id="responsive-navbar-nav">
+     <Nav className="mr-auto"></Nav>
+     <Nav>
+      <Nav.Link href="#deets">About</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes"> Services </Nav.Link>
+      <Nav.Link href="#deets" bsPrefix="lastItem">Contact us</Nav.Link>
+    </Nav>
+    <Button variant="outline-light">Login/Signup</Button>
+  </Navbar.Collapse>
+</Navbar>
+<Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100 img"
+      src={slide1}
+      alt="First slide"
+    />
+  </Carousel.Item>
 
-          </div>
-          <ul>
-              <li><a href=""></a></li>
-          </ul>
-      </div>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100 img"
+      src={slide1}
+      alt="Third slide"
+    />
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100 img"
+      src={slide2}
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
+</div>
     );
+}
 }
  
 export default Header;
