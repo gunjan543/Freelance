67c21e962ad5b62d6722a38fdf2862bdba58a5b6
gunjan543 <gunjanagarwal54321@gmail.com>
2020-12-30 12:25:46 +0530

    Added Future Advancements

---
+++ client/src/components/Dashboard/future.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './future.css';
+import Recommend from "../images/recommend.png";
+import Bid from "../images/bid.jpg";
+import Online from "../images/online.jpg";
+import Feedback from "../images/feedback.jpg";
+import logo from '../images/logo.png';
+const Future = () => {
+    return ( 
+        <div className="future">
+           <div className="future-logo"> <img src={logo}></img></div>
+           <a href="/home">Home</a>
+            <h1>
+                Future Advancements
+            </h1>
+            <div className="future-divs">
+                <img className="future-img" src={Recommend} alt = ""/>  
+                <p>Recommend suitable employee,according to the <br></br>needs of the employer</p>
+                <img className="future-img" src={Bid} alt = ""/>  
+                <p>Including Bid system for employees</p>
+                <img className="future-imgs" src={Online} alt = ""/>     
+                <p>Including Online Transactions</p>
+                <img className="future-imgs" src={Feedback} alt = ""/>  
+                <p>Customising Employee preferences <br></br>based on Employer Feedback</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Future;
