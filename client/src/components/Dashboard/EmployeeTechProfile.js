9cba3d69395667459dec3c7d6c53c91332062495
Gunjan Agarwal <gunjanagarwal54321@gmail.com>
2021-06-18 22:04:02 +0530

    Update EmployeeTechProfile.js

--- client/src/components/Dashboard/EmployeeTechProfile.js
+++ client/src/components/Dashboard/EmployeeTechProfile.js
@@ -7,25 +7,22 @@ import {removeCookie, removeLocalStorage} from '../../helpers/auth';
 
 export default class EmployeeTechProfile extends Component {
 
-  constructor(props){
+constructor(props){
   super();
   let { email} = JSON.parse(localStorage.getItem('user'));
   this.state = {
     email:email,
     user:{}
   }
-  }
-  
+ }
   componentDidMount(){
-
-    console.log(this.state.email);
    Axios
     .post(`/api/getUser`, this.state)
     .then( res=>{ 
      const user = res.data;
      this.setState({user});
-  }
-    )
+    }
+   )
   }
   render(){  
     const {history} = this.props;
@@ -37,7 +34,9 @@ export default class EmployeeTechProfile extends Component {
   return ( 
         <div>
          {!isAuth()?<Redirect to='/login'/> :null}
+         //navbar
         <Navbar2 />
+        //logout button  
         <button class="logoutBtn"  onClick = {signout}>
         <svg width="103px" height="41px"viewBox="0 0 180 60" class="border">
         <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
@@ -45,7 +44,7 @@ export default class EmployeeTechProfile extends Component {
         </svg>
         <span>Logout</span>
         </button>
-        
+        //profile details
         <div className="data"> 
         <div className="data-container"><h1>Profile</h1></div>
         <div className="data-container">
@@ -54,6 +53,7 @@ export default class EmployeeTechProfile extends Component {
         <div className="data-containers"><h4>Date of Birth - </h4><p>{this.state.user.dateOfBirth}</p></div>    
         <div className="data-containers"><h4>Gender -  </h4><p> {this.state.user.gender}</p></div>
         </div>
+        //contact details
         <div className="data-container">
         <h3>Contact Details  <i class="fa fa-address-book"></i></h3>
         <div className="data-containers"><h4>Email - </h4><p>{this.state.user.email}</p></div>
@@ -62,13 +62,14 @@ export default class EmployeeTechProfile extends Component {
         <div className="data-containers"><h4>Address-</h4><p>{this.state.user.address}</p></div>
         <div className="data-containers"><h4>Postal Code - </h4><p>{this.state.user.postalCode}</p></div>
         </div>
+        //skills
         <div className="data-container">
         <h3>Specialisation and Skills<i class="fa fa-graduation-cap"></i></h3>
         <div className="data-containers"><h4>Specialisation - </h4><p>{this.state.user.specialisation}</p></div>
         <div className="data-containers"><h4>Skills - </h4><p>{this.state.user.skills}</p></div>
         <div className="data-containers"><h4>Years of experience - </h4><p>{this.state.user.yearsOfExperience}</p></div>
         </div>
-       
+        //change password
         <div className="data-container">
         <h3>Change password  <i class="fa fa-key"></i></h3> 
         <div className="data-containers"><a href="/users/password/forget">Change password</a></div>
